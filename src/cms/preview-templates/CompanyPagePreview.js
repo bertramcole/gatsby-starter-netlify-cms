f0f195a302d4b3c87af938e2203d197ceb7ee18e
Bert Cole <bertramcole@hotmail.com>
2019-02-06 15:21:44 -0800

    Update CompanyPagePreview.js

--- src/cms/preview-templates/CompanyPagePreview.js
+++ src/cms/preview-templates/CompanyPagePreview.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { CompanyPageTemplate } from '../../templates/about-page'
+import { CompanyPageTemplate } from '../../templates/company-page'
 
 const CompanyPagePreview = ({ entry, widgetFor }) => (
   <CompanyPageTemplate
