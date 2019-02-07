db89d0deac418c1d5d38b044c7fab7fe6fc88285
Bert Cole <bertramcole@hotmail.com>
2019-02-06 18:59:25 -0800

    Update cms.js

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,10 +4,8 @@ import AboutPagePreview from './preview-templates/AboutPagePreview'
 import CompanyPagePreview from './preview-templates/CompanyPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
-import CompanyPagePreview from './preview-templates/CompanyPagePreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('company', CompanyPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('company', CompanyPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
