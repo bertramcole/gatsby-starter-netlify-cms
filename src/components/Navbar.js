8060d261170159d9e02cff1b4994fb32cf004501
Bert Cole <bertramcole@hotmail.com>
2019-02-07 12:06:15 -0800

    Logo & Color addition
    Demo Assest are In and about to attemp to change the index page to not be the blog feed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = class extends React.Component {
@@ -34,8 +33,8 @@ const Navbar = class extends React.Component {
   <nav className="navbar is-transparent" role="navigation" aria-label="main-navigation">
     <div className="container">
       <div className="navbar-brand">
-        <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+        <Link to="/company" className="navbar-item" title="Logo">
+          <img src={logo} alt="Kaldi" style={{ width: '200px' }} />
         </Link>
         {/* Hamburger menu */}
         <div className="navbar-burger burger" data-target="navMenu">
@@ -46,20 +45,20 @@ const Navbar = class extends React.Component {
       </div>
       <div id="navMenu" className="navbar-menu">
       <div className="navbar-start has-text-centered">
-        <Link className="navbar-item" to="/about">
+      <Link className="navbar-item" to="/company">
           About
-        </Link>
-        <Link className="navbar-item" to="/company">
-          Company
         </Link>
         <Link className="navbar-item" to="/products">
           Products
         </Link>
-        <Link className="navbar-item" to="/company">
+        <Link className="navbar-item" to="/about">
+          Roadmap
+        </Link>
+        <Link className="navbar-item" to="/contact">
           Contact
         </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
+        <Link className="navbar-item" to="/">
+          Newsroom
         </Link>
       </div>
       <div className="navbar-end has-text-centered">
@@ -69,9 +68,6 @@ const Navbar = class extends React.Component {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
         </a>
       </div>
       </div>
